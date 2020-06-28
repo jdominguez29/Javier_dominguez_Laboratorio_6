0ac4bce5f95f6489bcb86bccc982720fc2327f85
vagrant <vagrant@debian9.localdomain>
2020-06-28 01:31:30 +0000

    Actualizacion v1.2

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ const ESTUDIANTES = [
     {
         nombre: "Naresh Quiroz",
         edad: 22
+    },
+    {
+
     }
 ];
 //Intermediario
@@ -26,14 +29,28 @@ app.get('/Estudiantes/',(req, res) => {
     });
 });
 
-//Traer estudiante especifico
-app.get('/estudiantes/:indice', (req, res) => {
-    res.json(ESTUDIANTES[req.params.indice]);
-});
-
 //Agregar Estudiante
 app.post('/estudiantes/', (req, res) =>{
     ESTUDIANTES.push(req.body);
     res.json(req.body);
 });
+
+//Traer estudiante especifico
+app.get('/estudiantes/:indice', (req, res) => {
+    res.json(ESTUDIANTES[req.params.indice]);
+});
+
+// Actualizar campos de un estudiante
+app.put('/estudiantes/:indice', (req,res) => {
+    ESTUDIANTES[req.params.indice].nombre = req.body.nombre;
+    ESTUDIANTES[req.params.indice].edad = req.body.edad;
+    res.send("Estudiante Actualizado");
+ });
+ 
+ // Eliminar un estudiante de la lista 
+ app.delete('/estudiantes/:indice', (req,res) => {
+     ESTUDIANTES.splice(req.params.indice,1);
+     res.send("Estudiante Eliminado");
+  });
+
 app.listen(port,() => console.log('Example app listening at http://localhost:'+port));
